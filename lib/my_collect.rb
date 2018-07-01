c908e47aa43baa3638552df3bfe5de4684b25162
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-01 18:08:06 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(array)
+  index = 0 
+  new_array = [] 
+  while index < array.length 
+    yield array[index]
+    array[index].split.length == 2 ? new_array << array[index].split(' ').first : new_array << array[index].upcase  
+    index += 1
+  end 
+  new_array
+end
 
